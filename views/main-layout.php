39bfb39590084dbe8f8951c29ef928a6e5eb4d31
Vladimir <backend@div26.ru>
2018-03-16 11:27:32 +0300

    Remove CND dependency

--- views/main-layout.php
+++ views/main-layout.php
@@ -19,12 +19,6 @@ AlteAsset::register($this);
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <?= Html::csrfMetaTags() ?>
     <title><?= Html::encode($this->title) ?></title>
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-    <script src="//cdn.bootcss.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-    <script src="//cdn.bootcss.com/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
     <?php $this->head() ?>
 </head>
 <body class="fixed skin-blue">
